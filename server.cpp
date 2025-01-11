ea1329010165eee8794d4fbaa65e8e37e8ac3380
bazizi <bazizi@github.com>
2025-01-10 20:29:02 -0800

    separate client & server exes

---
+++ server.cpp
@@ -0,0 +1,70 @@
+#include "winrt/MyNamespace.h"
+#include "winrt/Windows.Foundation.h"
+#include "winrt/base.h"
+
+#include <wrl.h>
+#include <Windows.h>
+#include <wrl/module.h>
+#include <future>
+
+namespace winrt::MyNamespace
+{
+  template <typename T>
+    class SingletonFactory : public ::Microsoft::WRL::ClassFactory<> // NOSONAR - Inheritance hierarchy depth of 6 and above is required by winrt
+  {
+    public:
+      IFACEMETHODIMP CreateInstance(::IUnknown* unknownOuter, REFIID riid, void** object) noexcept override // NOSONAR - winrt requires void type
+      {
+        if (object == nullptr)
+        {
+          return CO_E_CLASS_CREATE_FAILED;
+        }
+
+        if (unknownOuter != nullptr)
+        {
+          return CLASS_E_NOAGGREGATION;
+        }
+
+        *object = nullptr;
+        return T::Instance().as(riid, object);
+      }
+  };
+
+  struct 
+    __declspec(uuid("ddc36e02-18ac-47c4-ae17-d420eece2281"))
+    MyInterfaceImp : implements<MyInterfaceImp, IMyInterface>
+    {
+      static winrt::com_ptr<winrt::MyNamespace::MyInterfaceImp> Instance();
+
+      winrt::hstring GetUsername()
+      {
+        WCHAR buffer[1024] = {};
+        DWORD cBuffer = sizeof(buffer)/sizeof(*buffer);
+        GetUserNameW(buffer, &cBuffer);
+        return winrt::hstring{ buffer };
+      }
+
+      MyInterfaceImp()
+      {}
+    };
+
+  CoCreatableClassWithFactory(MyInterfaceImp, SingletonFactory<MyInterfaceImp>);
+
+
+  winrt::com_ptr<MyInterfaceImp> MyInterfaceImp::Instance()
+  {
+    return winrt::make_self<MyInterfaceImp>();
+  }
+} // winrt::MyNamespace
+
+int main (int argc, char *argv[])
+{
+  winrt::init_apartment();
+
+  std::promise<void> promise;
+  auto& mModule = ::Microsoft::WRL::Module<::Microsoft::WRL::OutOfProc>::Create([&promise](){promise.set_value();});
+  mModule.RegisterObjects();
+  promise.get_future().get();
+
+  return 0;
+}
